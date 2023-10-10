cc23ddbd2fd06d8e978076e8f219b5c50f5339ec
Firestrike05 <z41yn.94@gmail.com>
2023-10-10 09:29:52 +0100

    0.0.2

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Declare scanner
         Scanner s = new Scanner(System.in);
         Double num1;
+        //Take the first argument to the calculator
         num1 = s.nextDouble();
         s.nextLine();
+        //Prompt the user to select a calculator function
         System.out.println("Operation (+, -, /, x): ");
         String choice = s.nextLine();
 
-        //Create an array list to take user input
-        ArrayList<Double> num2 = new ArrayList<Double>();
+        //Create an array list filled with user input and repeat until no more arguments are needed
+        ArrayList<Double> num2 = new ArrayList<>();
         while (s.hasNextLine()) {
             String line = s.nextLine().trim();
             if (line.isEmpty()) {
@@ -21,13 +24,15 @@ public class Main {
                 double i = Double.parseDouble(line);
                 num2.add(i);
             } catch (NumberFormatException e) {
+                //Throw an error if user doesn't enter a number
                 System.out.println("Invalid input: " + line);
             }
         }
 
+        //Convert the arraylist to a Double array for use with a Vararg argument
         Double[] num3 = num2.toArray(new Double[num2.size()]);
-        double result;
 
+        //Switch statements that take user inputted function and prints results of calculation
         switch (choice) {
             case "-":
                 Vararg.sub(num1, num3);
@@ -46,7 +51,7 @@ public class Main {
                 System.out.println("Invalid choice");
                 break;
         }
-
+        //Prints out the arguments entered by the user
         System.out.println(num1);
         System.out.println(num2);
 
